fec1950017419cae636e31a54c0710c0a759a2b9
Md Nazare Alam <79821343+itsnazare@users.noreply.github.com>
2022-07-19 18:23:44 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var Typed = new typed(".typing", {
+    strings: ["Java Developer","Front-End Developer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: ture
+});
+function dark(){
+    var dark = document.body;
+    dark.classList.toggle("dark-mode");
+
+}
